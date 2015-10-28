c639a59646259177e959373e8942b8f81540c101
Yoojia.Chen <yoojia.chen@gmail.com>
2015-10-28 23:06:32 +0800

    添加Subscriber事件帮助类

--- next-android/src/main/java/com/github/yoojia/next/events/Events.java
+++ next-android/src/main/java/com/github/yoojia/next/events/Events.java
@@ -6,5 +6,27 @@ package com.github.yoojia.next.events;
  */
 final public class Events {
 
+    public static Object[] on1(String event, Class<?> type) {
+        return new Object[]{event, type};
+    }
 
+    public static Object[] on2(String event1, Class<?> type1, String event2, Class<?> type2) {
+        return new Object[]{event1, type1, event2, type2};
+    }
+
+    public static Object[] on3(String event1, Class<?> type1, String event2, Class<?> type2,
+                               String event3, Class<?> type3) {
+        return new Object[]{event1, type1, event2, type2, event3, type3};
+    }
+
+    public static Object[] on4(String event1, Class<?> type1, String event2, Class<?> type2,
+                               String event3, Class<?> type3, String event4, Class<?> type4) {
+        return new Object[]{event1, type1, event2, type2, event3, type3, event4, type4};
+    }
+
+    public static Object[] on5(String event1, Class<?> type1, String event2, Class<?> type2,
+                               String event3, Class<?> type3, String event4, Class<?> type4,
+                               String event5, Class<?> type5) {
+        return new Object[]{event1, type1, event2, type2, event3, type3, event4, type4, event5, type5};
+    }
 }

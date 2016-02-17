8b4fe2521a19d6e1b0d1f136e39fd0a24716b05e
YooJia.Chen <yoojiachen@gmail.com>
2016-02-17 15:12:28 +0800

    1.0-beta

--- app/src/main/java/com/github/yoojia/next/app/MainActivity.java
+++ app/src/main/java/com/github/yoojia/next/app/MainActivity.java
@@ -2,7 +2,6 @@ package com.github.yoojia.next.app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,10 +9,6 @@ import com.github.yoojia.next.clicks.Click;
 import com.github.yoojia.next.clicks.ClickEvent;
 import com.github.yoojia.next.clicks.ClickHandler;
 import com.github.yoojia.next.clicks.NextClickProxy;
-import com.github.yoojia.next.events.Runs;
-import com.github.yoojia.next.events.Subscribe;
-import com.github.yoojia.next.flux.Action;
-import com.github.yoojia.next.flux.Dispatcher;
 import com.github.yoojia.next.views.BindView;
 import com.github.yoojia.next.views.NextBindView;
 
@@ -28,10 +23,6 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.button)
     private Button mButton;
 
-    private final Dispatcher mDispatcher = new Dispatcher();
-
-    private TestStore mStore;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,31 +31,17 @@ public class MainActivity extends AppCompatActivity {
         NextBindView.use(this).inject(this);
         // Click proxy
         NextClickProxy.bind(this);
-        // Flux
-        mStore = new TestStore(mDispatcher);
-        mDispatcher.register(mStore);
-        mDispatcher.register(this);
-    }
 
-    @Subscribe(on = ActionTypes.CHANGED_MESSAGES, run = Runs.ON_CALLER)
-    public void onChanged(Action action) {
-        Log.d(TAG, "- Emit event, finish");
     }
 
     @ClickHandler(on = "click")
     private void onClick(ClickEvent<Button> evt) {
-        long emitStart = System.currentTimeMillis();
-        long longData = System.currentTimeMillis();
-        mDispatcher.emit(ActionTypes.createRawMessage(new LongMessage(longData)));
-        long diff = System.currentTimeMillis() - emitStart;
-        Log.d(TAG, "- Emit an event, takes: " + diff + "ms");
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mDispatcher.unregister(mStore);
-        mDispatcher.unregister(this);
     }
 
 }
